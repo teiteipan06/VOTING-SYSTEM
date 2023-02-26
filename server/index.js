729156dc4c3b161c7b90ee6c09a5d950c54a925d
teiteipan06 <y7321543@gmail.com>
2023-02-26 23:29:43 +0900

    test hello

--- server/index.js
+++ server/index.js
@@ -1,3 +1,15 @@
 const express = require("express");
 const fs = require("fs");
 const path = require("path");
+
+const app = express();
+const dataFile = path.join(__dirname, "data.json");
+
+// Support POSTing form data with URL encoded
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/poll", (req, res) => {
+  res.send("Hello");
+});
+
+app.listen(3000, () => console.log("Server is running..."));
