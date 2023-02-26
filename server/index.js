820d10ef756aeeb1e33ebe191368d5dcc4f9a4b0
teiteipan06 <y7321543@gmail.com>
2023-02-26 23:47:15 +0900

    test server

--- server/index.js
+++ server/index.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const fs = require("fs");
+const fs = require("fs").promises;
 const path = require("path");
 
 const app = express();
@@ -8,8 +8,12 @@ const dataFile = path.join(__dirname, "data.json");
 // Support POSTing form data with URL encoded
 app.use(express.urlencoded({ extended: true }));
 
-app.get("/poll", (req, res) => {
-  res.send("Hello");
+app.get("/poll", async (req, res) => {
+  let data = JSON.parse(await fs.readFile(dataFile, "utf-8"));
+
+  console.log(data);
+
+  res.end();
 });
 
-app.listen(3000, () => console.log("Server is running..."));
+app.listen(3000, () => console.log("Server is running...")); 
